fd34cdcbcf3d675872f1fad223999acae06a712f
Preeti Amatya <preetiamatya@gmail.com>
2016-05-21 17:03:45 +0200

    removed unnecessary log

--- hyena.js
+++ hyena.js
@@ -1,8 +1,7 @@
 
 var app = angular.module('hyApp', []);
 app.controller('hyCtrl', function($scope) {
-    $scope.listquery = '';
-	console.log("Setting names");
+    $scope.listquery = '';	
     $scope.list = ["Abhinav", "A M Publishers", "Academe Research Journals", "Academia Publishing", "Academic and Business Research Institute", "Academic Journals", "Academic Journals, Inc.","Academic Journals and Research ACJAR"];
 });
 
